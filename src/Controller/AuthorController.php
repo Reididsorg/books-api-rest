36632541cd44e90180451cd7fcf5170e8e915bc7
bruno.grosdidier@2dcom.fr <bruno.grosdidier@2dcom.fr>
2026-03-09 12:23:54 +0100

    Ajout des contrôleurs 'allAuthors' et 'author'

---
+++ src/Controller/AuthorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+final class AuthorController extends AbstractController
+{
+    #[Route('/api/authors', name: 'allAuthors')]
+    public function getAllAuthors(AuthorRepository $authorRepository, SerializerInterface $serializer): JsonResponse
+    {
+        $authorList = $authorRepository->findAll();
+
+        $jsonAuthorList = $serializer->serialize($authorList, 'json', ['groups' => 'getAuthors']);
+
+        return new JsonResponse($jsonAuthorList, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('api/authors/{id}', name: 'author')]
+    public function getAuthor(Author $author, SerializerInterface $serializer): JsonResponse {
+        $jsonAuthor = $serializer->serialize($author, 'json', ['groups' => 'getAuthors']);
+        return new JsonResponse($jsonAuthor, Response::HTTP_OK, [], true);
+    }
+}
