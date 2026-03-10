fde02f77ac3cad5fdf20d23282bd2eeb0721880c
bruno.grosdidier@2dcom.fr <bruno.grosdidier@2dcom.fr>
2026-03-10 14:00:51 +0100

    Ajout des fixtures de User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,36 @@ namespace App\DataFixtures;
 
 use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+        // Création d'un user "normal"
+        $user = new User();
+        $user->setEmail("user@books-api-rest.local");
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, "12345"));
+        $manager->persist($user);
+
+        // Création d'un user admin
+        $userAdmin = new User();
+        $userAdmin->setEmail("admin@books-api-rest.local");
+        $userAdmin->setRoles(["ROLE_ADMIN"]);
+        $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "12345"));
+        $manager->persist($userAdmin);
+
         // Création des auteurs.
         $listAuthor = [];
         for ($i = 0; $i < 10; $i++) {
